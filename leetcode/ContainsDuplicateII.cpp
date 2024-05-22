975d79de661541c06ad6815f8712f6088e898224
Shakthi-Viviyn <55011804+Shakthi-Viviyn@users.noreply.github.com>
2024-05-22 12:34:23 -0400

    finished contains duplicates

---
+++ leetcode/ContainsDuplicateII.cpp
@@ -0,0 +1,27 @@
+#include <unordered_set>
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+        std::unordered_set<int> s;
+        int i = 0;
+        while(i <= k && i < nums.size()){
+            if (s.find(nums[i]) != s.end()){
+                return true;
+            }
+            s.insert(nums[i]);
+            i++;
+        }
+
+        while(i < nums.size()){
+            s.erase(nums[i - (k + 1)]);
+            if (s.find(nums[i]) != s.end()){
+                return true;
+            }
+            s.insert(nums[i]);
+            i++;
+        }
+        return false;
+    }
+};
